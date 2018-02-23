13a0b43496f61085562691df5687888a79d33779
Alain Gilbert <alain.gilbert.15@gmail.com>
2018-02-22 16:38:51 -0800

    Add useful functions

--- src/minecraft/manticore/Manticore.java
+++ src/minecraft/manticore/Manticore.java
@@ -10,4 +10,15 @@ public class Manticore {
     public static boolean xrayIronOre = true;
     public static boolean xrayGoldOre = true;
     public static boolean xrayBedrock = true;
+
+    public static float getPct(float min, float max, float val) {
+        float pct = (val - min) / max;
+        if (pct < 0F) pct = 0F;
+        if (pct > 1F) pct = 1F;
+        return pct;
+    }
+
+    public static float interpolate(float from, float to, float pct) {
+        return from + (to - from) * pct;
+    }
 }
